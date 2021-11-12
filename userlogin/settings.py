b21bf9eac0a13856786a25882ea493141b0b62ad
RohitPawarFS <rohitpawarrmp495@gmail.com>
2021-11-12 07:18:04 +0530

    update

--- userlogin/settings.py
+++ userlogin/settings.py
@@ -14,7 +14,17 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 
 # Quick-start development settings - unsuitable for production
